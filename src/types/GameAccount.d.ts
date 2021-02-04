af04c78584a45e0caadcb7dbc5677a5c70c1a362
Krzysztof Saczuk <zakuciael@outlook.com>
2021-02-04 01:59:37 +0100

    Added RawGameAccount and GameAccount interfaces.

---
+++ src/types/GameAccount.d.ts
@@ -0,0 +1,37 @@
+interface RawGameInfo {
+    game: string;
+    server: string;
+    user: string;
+    lang: string;
+}
+
+interface RawCurrency {
+    amount: number;
+    currencyId: string;
+    locaKey: string;
+}
+
+export interface RawGameAccount {
+    id: string;
+    clientId: string;
+    gameId: string;
+    gameEnvironmentId: string;
+    userId: string;
+    accountGroup: string;
+    displayName: string;
+    usernames: string[];
+    created: string; // Date
+    deleted: string | null; // Date
+    preDeleted: string | null; // Date
+    lastLogin: string | null; // Date
+    accountNumericId: number;
+    guls: RawGameInfo;
+    wallet: { currencies: RawCurrency[] };
+}
+
+export interface GameAccount {
+    id: string;
+    accountName: string;
+    usernames: string[];
+    game: string;
+}
