531de248cdf32e33d20730fc7eb0476042571bc5
Krzysztof Saczuk <zakuciael@outlook.com>
2021-02-07 02:01:57 +0100

    Added index file for lib/types/ directory.

---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+export { GameAccount, GameInfo } from "./GameAccount";
+export * from "./GameforgeClientReleaseVersions";
+export * from "./GameforgeClientVersion";
+export * from "./GetAccountTokenOptions";
+export * from "./CharType";
