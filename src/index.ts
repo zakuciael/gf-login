df1bb5fbe7c01444c3eac10e92b1ab906ae5d7d1
Krzysztof Saczuk <zakuciael@outlook.com>
2021-02-07 02:03:28 +0100

    Added main index file with all exports.

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export { getGameforgeClientVersion } from "./lib/getGameforgeClientVersion";
+export { createAccountHash } from "./lib/utils/createAccountHash";
+export { sendStartTimeEvent } from "./lib/sendStartTimeEvent";
+export { getAccountToken } from "./lib/getAccountToken";
+export { getGameAccounts } from "./lib/getGameAccounts";
+export { getGameToken } from "./lib/getGameToken";
+export * from "./lib/errors";
+export * from "./lib/tokens";
+export * from "./types";
