174b8b37bbc624b0e9463d5b4aaa00bf06324f3f
Krzysztof Saczuk <zakuciael@outlook.com>
2021-02-07 01:52:14 +0100

    Added few crypto-related utility methods.

---
+++ src/lib/utils/crypto.ts
@@ -0,0 +1,19 @@
+import { createHash } from "crypto";
+import { CharType } from "../../types/CharType";
+
+const hash = (data: Buffer | string, algorithm: string, upper: boolean): string => {
+    const hash = createHash(algorithm).update(data).digest("hex");
+    return upper ? hash.toUpperCase() : hash;
+};
+
+export const sha1 = (data: Buffer | string, upper = false): string => {
+    return hash(data, "sha1", upper);
+};
+
+export const sha256 = (data: Buffer | string, upper = false): string => {
+    return hash(data, "sha256", upper);
+};
+
+export const getCharFromType = (type: CharType): string => {
+    return type === CharType.RETURN_C ? "C" : type === CharType.RETURN_F ? "F" : "";
+};
---
+++ src/types/CharType.ts
@@ -0,0 +1,4 @@
+export enum CharType {
+    RETURN_C = 0,
+    RETURN_F = 1,
+}
