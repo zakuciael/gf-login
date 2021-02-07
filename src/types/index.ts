bd1433610824630c14bdf9e62b646f8ab3c37572
Krzysztof Saczuk <zakuciael@outlook.com>
2021-02-07 02:17:07 +0100

    Removed CharType from types/ directory exports.

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,4 +2,3 @@ export { GameAccount, GameInfo } from "./GameAccount";
 export * from "./GameforgeClientReleaseVersions";
 export * from "./GameforgeClientVersion";
 export * from "./GetAccountTokenOptions";
-export * from "./CharType";
