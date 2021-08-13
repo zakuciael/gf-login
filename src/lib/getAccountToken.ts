e3ab35b51ea28606e8fd5ba4a2eb75b2683168a1
Gorlik <33355238+GorlikItsMe@users.noreply.github.com>
2021-08-13 08:40:50 +0200

    Set missing User-Agent in header

--- src/lib/getAccountToken.ts
+++ src/lib/getAccountToken.ts
@@ -34,6 +34,7 @@ export const getAccountToken = (
         headers: Object.assign(
             {
                 "Content-Type": "application/json",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36",
                 "TNT-Installation-Id": installationID,
             },
             options.challengeId ? { "gf-challenge-id": options.challengeId } : undefined
